2b4ee967cc90252cad16352075e4a13f7fd4cbc0
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2022-08-03 14:28:35 -0400

    Added fonttools to the toolchain

--- build/make.d/10-gen-fonts.bash
+++ build/make.d/10-gen-fonts.bash
@@ -4,7 +4,7 @@
 ## the GNU AGPL 3.0 with the additional allowances of the GNU LGPL 3.0.
 
 function genFont {
-	fontforge -script 'build/common/genFont.py' "$1" "$2" "$3" 2>&1 | tail -n +4 1>&2
+	fontforge -script 'build/common/genFont.py' "$1" "$2" "$3" 2>/dev/null 1>/dev/null
 }
 
 echo -e "\e[34;1m::\e[0;1m Generating fonts...\e[0m"
@@ -15,7 +15,7 @@ for F in $(find -type f); do
 	cd "$CWD"
 	F="$(echo $F | sed 's/^.*[/]//gm' | sed 's/[.]sfd$//gm')"
 	if [[ "$F" != '.'* ]] && [[ "$F" != *'~' ]]; then
-		echo -e "\e[34;1m::\e[0m Compiling and hinting .ttf..."
+		echo -e "\n\e[34;1m::\e[0m Compiling .ttf..."
 		echo "bin/$F.ttf"
 
 		## Compile a .ttf
@@ -25,7 +25,13 @@ for F in $(find -type f); do
 			exit 1
 		fi
 
-		## Hint that .ttf
+		## Optimize the .ttf
+		echo -e "\e[34;1m::\e[0m Optimizing .ttf..."
+		fonttools otlLib.optimize "bin/$F.ttf"
+		fonttools cffLib.width "bin/$F.ttf"
+
+		## Hint the .ttf
+		echo -e "\e[34;1m::\e[0m Hinting .ttf..."
 		ttfautohint -iW --default-script=latn --fallback-script=latn --fallback-stem-width=100 --hinting-limit=96 --hinting-range-max=36 --hinting-range-min=5 --increase-x-height=12 --stem-width-mode=qsq "bin/$F.ttf" "bin/Hinted_$F.ttf" #-p
 		if [[ ! -f "bin/Hinted_$F.ttf" ]]; then
 			echo 'Failed to hint font!' >&2
@@ -33,15 +39,19 @@ for F in $(find -type f); do
 		fi
 		mv "bin/Hinted_$F.ttf" "bin/$F.ttf"
 
-		## Save the modified font
+		## Compile an .otf from the .ttf
+		echo -e "\n\e[34;1m::\e[0m Compiling .otf..."
 		genFont 'rip' "bin/$F.ttf" "bin/$F.sfd"
-
-		## Compile an .otf
-		echo -e "\e[34;1m::\e[0m Compiling .otf from .ttf..."
 		echo "bin/$F.otf"
 		genFont 'gen' "bin/$F.sfd" "bin/$F.otf"
 		rm -f "bin/$F.sfd"
+
+		## Optimize the .otf
+		echo -e "\e[34;1m::\e[0m Optimizing .otf..."
+		fonttools otlLib.optimize "bin/$F.otf"
+		fonttools cffLib.width "bin/$F.otf"
+		# fonttools subset "bin/$F.otf" #TODO: Come up with a good list of arguments for this.
 	fi
 done
-echo -e "\e[34;1m::\e[0;1m Done.\e[0m"
+echo -e "\n\e[34;1m::\e[0;1m Done.\e[0m"
 exit 0
